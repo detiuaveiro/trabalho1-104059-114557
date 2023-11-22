9a00518efec73e78e0d0a0f41ada509e1fab7163
GustavoGG2002 <117271145+GustavoGG2002@users.noreply.github.com>
2023-11-22 17:55:29 +0000

    Update image8bit.c

--- image8bit.c
+++ image8bit.c
@@ -249,11 +249,11 @@ void ImageStats(Image img, uint8 *min, uint8 *max)
     uint8 pixel_value = img->pixel[i];
     if (pixel_value < *min)
     {
-      *min = pixel_value-0.5;
+      *min = pixel_value;
     }
     if (pixel_value > *max)
     {
-      *max = pixel_value-0.5;
+      *max = pixel_value;
     }
   }
 }
